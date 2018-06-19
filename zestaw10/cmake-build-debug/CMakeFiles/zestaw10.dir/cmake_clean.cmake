6fceaf51d27a8c2087391b52e90dbaf7c496244f
dyduch <dominik.dyduch@gmail.com>
2018-06-19 17:09:58 +0200

    Initial commit

---
+++ zestaw10/cmake-build-debug/CMakeFiles/zestaw10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zestaw10.dir/main.c.o"
+  "zestaw10.pdb"
+  "zestaw10"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zestaw10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
